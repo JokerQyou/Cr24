4cd196fdb67e0165bd649606f8c90bb6ab2fd213
Joker Qyou <Joker.Qyou@gmail.com>
2016-06-23 23:28:58 +0800

    Improve newtab

--- scripts/background.js
+++
@@ -1,8 +0,0 @@
-if(chrome.tabs.onMouseScroll){
-  chrome.tabs.onMouseScroll.addListener(function(e){
-    console.info(e);
-  });
-}
-chrome.tabs.onCreated.addListener(function(tab){
-  // Boost new tab creation (literally, but you lost top sites).
-});
--- scripts/newtab.js
+++ scripts/newtab.js
@@ -1,8 +1,29 @@
-chrome.topSites.get(function(sites){
-  new Vue({
-    el: '#app',
-    data: {
-      sites: sites
-    }
-  });
+Vue.use(VueAsyncData);
+Vue.component('top-sites', {
+  template: multiline(function(){/*@preserve
+    <ol>
+      <li v-for="site in sites">
+        <a href="{{ site.url }}">
+          <img src="chrome://favicon/{{ site.url }}">
+          {{ site.title }}
+        </a>
+      </li>
+    </ol>,
+  */}),
+  data: function(){
+    return {
+      sites: ['Loading...']
+    };
+  },
+  asyncData: function(resolve, reject){
+    chrome.topSites.get(function(sites){
+      resolve({
+        sites: sites
+      });
+    });
+  }
+});
+
+new Vue({
+  el: 'body'
 });
